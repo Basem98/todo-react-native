d64a202c9ab87307a5efd4c4cf769a1a651c56ea
Basem98 <basem.mostafa98@hotmail.com>
2022-08-05 08:56:06 +0200

    feat: Implement initial application

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignInForm from './components/SignInForm/SignInForm';
+import ToDos from './components/ToDos/ToDos';
+import palette from './styles/palette';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerTitleAlign: 'center', headerTitleStyle: { color: palette.highlightOnSecond } }}>
+        <Stack.Screen name="Sign In" component={SignInForm}></Stack.Screen>
+        <Stack.Screen name='ToDos' component={ToDos}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
